f82ed02abba886e1b9ef973efd9999310def0016
baspram <bagaskarapramudita@gmail.com>
2014-05-12 23:10:11 +0700

    Add kelas Ajuna nih

---
+++ Arjuna.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Player;
+
+/**
+ *
+ * @author Ahmad
+ */
+/* Arjuna is one of WayangRace characters */
+public class Arjuna extends Player{
+    /* Arjuna's description */
+    final String description = "Arjuna bisa terbang rek";
+    
+    /* Action of arjuna */
+    public void useAction(){
+        //Lalala put your code's here 
+    }
+    /* show description */
+    public String getDescription(){
+        return description;
+    }
+}
