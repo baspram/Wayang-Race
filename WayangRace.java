d4ea1faf43df160886739e16dd0555feecf1c5ac
jeffhorus <jeffhorus19@gmail.com>
2014-05-19 22:48:52 +0700

    File hari H, tambahan main menu

---
+++ WayangRace.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author idea
+ */
+public class WayangRace {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame W = new NewJFrame();
+        W.show();
+    }
+    
+}
